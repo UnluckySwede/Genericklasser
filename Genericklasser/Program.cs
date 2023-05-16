58b58f5dc3ea5e3b504afc90b88c8e2e344e49c0
UnluckySwede <gadler.michael@elev.ga.ntig.se>
2023-05-16 12:55:58 +0200

    Generiska klasser

---
+++ Genericklasser/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+// List<string> namelist = new List<string>(); // Kan innehålla flera klassinstanser/datatyper
+
+// Queue<string> productionLine = new Queue<string>(); // En kö av strings
+
+// productionLine.Enqueue("boll"); // Lägger till vid slutet
+
+// productionLine.Dequeue(); // Tar bort från början och returnerar
+
+// Random generator = new Random();
+// Queue<int> numbers = new Queue<int>();
+// int i = 0;
+
+// while (i < 10)
+// {
+//     int number()
+//     {
+//         return generator.Next(0, 100);
+//     }
+
+//     numbers.Enqueue(number());
+
+//     int randomNumbers()
+//     {
+//         return numbers.Dequeue();
+//     }
+
+//     Console.WriteLine(randomNumbers());
+
+//     i++;
+// }
+// Console.ReadLine();
+
+
+// Dictionary<string, string> ordbok = new();
+// Boolean playing = true;
+
+// ordbok.Add("tiger", "Rovdjur");
+// ordbok.Add("val", "Stor sak");
+// ordbok.Add("katt", "Snabb");
+// ordbok.Add("hund", "Bruh");
+
+// while (playing == true)
+// {
+//     Console.WriteLine("Search for a words meaning.");
+
+//     string ord = Console.ReadLine().ToLower();
+
+//     if (ordbok.ContainsKey(ord))
+//     {
+//         Console.WriteLine(ordbok[ord]);
+//     }
+
+//     Console.ReadLine();
+
+// }
